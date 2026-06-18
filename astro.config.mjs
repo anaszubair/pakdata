2578ebb4f1cc49b95cb31d62b6082c590666870c
Anas Zubair <anas.zubair@pakdata.com>
2026-06-18 10:29:42 +0500

    add urdu urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
-// @ts-check
+// astro.config.mjs
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    redirects: {
+        '/ur/قرآن-مجید-کے-لیے-عربی-فونٹ/': '/ur/products/arabicfont',
+        '/ur/قرآن-ایکسپلورر/': '/ur/projects/quranexplorer',
+        '/ur/ايزى-اردو-کی-بورڈ/': '/ur/products/easyurdu',
+        '/ur/امیجن/': '/ur/projects/imagine',
+        '/ur/کتابی/': '/ur/products/kitabi',
+        '/ur/نیشنل-ایلیئن-ریجسٹریشن-ایتھاریٹی-nara/': '/ur/projects/nara-national-alien-registration-authority',
+        '/ur/تعارف/': '/ur/about-us',
+        '/ur/قرآن-پرنٹنگ-کامپلیکس-پروجیکٹ/': '/ur/projects/quran-printing-complex-project',
+        '/ur/قرآن-پاک/': '/ur/products/quran-pak',
+        '/ur/قرآن-آڈیو/': '/ur/quran-audio',
+        '/ur/قرآن-مجید/': '/ur/products/quran-majeed'
+    },
+});
